489cb68702e93472c5b3ee4e8d58d0fa9132e1fd
Joshua Daniel <joshua_dup@hotmail.com>
2025-01-20 12:18:58 -0500

    termino estructura html del template en el componente

--- src/components/ComicViewer.vue
+++ src/components/ComicViewer.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <h1>Random Comics</h1>
+    <h6>View your favorite comics at random and rate them as you like</h6>
+  </div>
+  <div>
+    <div>
+      <div>
+        <button>Get Random Comic</button>
+      </div>
+      <div>
+        <div>
+          <div>
+            <h2>Comic Title</h2>
+            <div>
+              <p>Comic Number</p>
+              <p>Comic Year</p>
+            </div>
+          </div>
+          <img src="" alt="" />Comic Image
+          <p>Comic Description</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
